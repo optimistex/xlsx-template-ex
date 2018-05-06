180e0b0a37a486c33d5ecb8f6a41fb70ad9e3f82
optimistex <optimistex@gmail.com>
2018-05-06 06:33:40 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,8 +1,85 @@
-let fs = require('fs');
-let xlsx = require('xlsx-populate');
+const fs = require('fs');
+const xlsx = require('xlsx-populate');
 
+function xlsxBuildByTemplate(templateFileName, data) {
+    return xlsx.fromFileAsync(templateFileName).then(function (wb) {
+        for (let name in data) {
+            if (data.hasOwnProperty(name)) {
+                if (typeof data[name] === 'string') {
+                    wb.find(`%${name}%`, data[name]);
+                } else if (typeof data[name] === 'object') {
+                    const curSheet = wb.find(`%${name}:begin%`)[0].row().sheet();
 
-let data = {
+                    const firstRowNum = wb.find(`%${name}:begin%`)[0].row().rowNumber() + 1;
+
+                    const lastRowNum = wb.find(`%${name}:end%`)[0].row().rowNumber();
+                    const repeatOffset = lastRowNum - firstRowNum;
+                    const totalObjCnt = data[name].length;
+                    const totalOffset = (totalObjCnt - 1) * repeatOffset;
+                    const maxColumn = curSheet.usedRange()._maxColumnNumber;
+                    const maxRow = curSheet.usedRange()._maxRowNumber;
+                    for (let r = maxRow; r > lastRowNum; r--) {
+                        for (let c = 1; c <= maxColumn; c++) {
+                            const curVal = curSheet.row(r).cell(c).value();
+                            const curStyle = curSheet.row(r).cell(c)._styleId;
+                            curSheet.row(r + totalOffset).cell(c).value(curVal);
+                            curSheet.row(r + totalOffset).cell(c)._styleId = curStyle;
+                        }
+                    }
+                    for (let i = 0; i < totalObjCnt; i++) {
+                        const curFirstRowNum = firstRowNum + i * repeatOffset;
+                        const curLastRowNum = lastRowNum + i * repeatOffset;
+                        let defRcnt = 0;
+                        for (let r = curFirstRowNum; r < curLastRowNum; r++) {
+                            for (let c = 1; c <= maxColumn; c++) {
+                                const tpltVal = curSheet.row(firstRowNum + defRcnt).cell(c).value();
+                                const tpltStyleId = curSheet.row(firstRowNum + defRcnt).cell(c)._styleId;
+                                let newTemplated = '';
+                                if (tpltVal !== undefined) {
+                                    if ((/%.*\..*%/gi).test(tpltVal)) {
+                                        newTemplated = '';
+                                        if (i === 0) {
+                                            newTemplated = tpltVal;
+                                        } else {
+                                            newTemplated = `%${tpltVal.slice(1, -1)}.${i}%`;
+                                            let re = new RegExp(`%${name}\.[^%]*%`, 'gi');
+                                            newTemplated = tpltVal.replace(re, function (match) {
+                                                return `%${match.slice(1, -1)}.${i}%`;
+                                            });
+                                        }
+                                    } else {
+                                        newTemplated = tpltVal;
+                                    }
+                                } else {
+                                    newTemplated = undefined;
+                                }
+                                curSheet.row(r).cell(c).value(newTemplated);
+                                curSheet.row(r).cell(c)._styleId = tpltStyleId;
+
+                            }
+                            defRcnt++;
+                        }
+                    }
+                    for (let i = 0; i < totalObjCnt; i++) {
+                        for (let attr in data[name][i]) {
+                            if (data[name][i].hasOwnProperty(attr)) {
+                                if (i === 0) {
+                                    wb.find(`%${name}.${attr}%`, data[name][i][attr]);
+                                } else {
+                                    wb.find(`%${name}.${attr}.${i}%`, data[name][i][attr]);
+                                }
+                            }
+                        }
+                    }
+                    wb.find(`%${name}:begin%`, '');
+                }
+            }
+        }
+        return wb.outputAsync('buffer');
+    });
+}
+
+const testData = {
     'name': 'Отчёт',
     'date': '01.05.2018',
     'users': [
@@ -41,82 +118,8 @@ let data = {
     ]
 };
 
-
-xlsx.fromFileAsync('test.xlsx').then(function (wb) {
-    for (name in data) {
-        if (typeof data[name] === 'string') {
-            wb.find(`%${name}%`, data[name]);
-        } else if (typeof data[name] === 'object') {
-            let curSheet = wb.find(`%${name}:begin%`)[0].row().sheet();
-
-            let firstRowNum = wb.find(`%${name}:begin%`)[0].row().rowNumber() + 1;
-
-            let lastRowNum = wb.find(`%${name}:end%`)[0].row().rowNumber();
-            let repeatOffset = lastRowNum - firstRowNum;
-            let totalObjCnt = data[name].length;
-            let totalOffset = (totalObjCnt - 1) * repeatOffset;
-            let maxColumn = curSheet.usedRange()._maxColumnNumber;
-            let maxRow = curSheet.usedRange()._maxRowNumber;
-            for (let r = maxRow; r > lastRowNum; r--) {
-                for (let c = 1; c <= maxColumn; c++) {
-                    let curVal = curSheet.row(r).cell(c).value();
-                    let curStyle = curSheet.row(r).cell(c)._styleId;
-                    curSheet.row(r + totalOffset).cell(c).value(curVal);
-                    curSheet.row(r + totalOffset).cell(c)._styleId = curStyle;
-                }
-            }
-            for (let i = 0; i < totalObjCnt; i++) {
-                let curFirstRowNum = firstRowNum + i * repeatOffset;
-                let curLastRowNum = lastRowNum + i * repeatOffset;
-                let defRcnt = 0;
-                for (let r = curFirstRowNum; r < curLastRowNum; r++) {
-                    for (let c = 1; c <= maxColumn; c++) {
-                        let tpltVal = curSheet.row(firstRowNum + defRcnt).cell(c).value();
-                        let tpltStyleId = curSheet.row(firstRowNum + defRcnt).cell(c)._styleId;
-                        if (tpltVal !== undefined) {
-                            if ((/%.*\..*%/gi).test(tpltVal)) {
-                                let newTemplated = '';
-                                if (i === 0) {
-                                    newTemplated = tpltVal;
-                                } else {
-                                    newTemplated = `%${tpltVal.slice(1, -1)}.${i}%`;
-                                    let re = new RegExp(`%${name}\.[^%]*%`, 'gi');
-                                    newTemplated = tpltVal.replace(re, function (match) {
-                                        return `%${match.slice(1, -1)}.${i}%`;
-                                    });
-                                }
-                            } else {
-                                newTemplated = tpltVal;
-                            }
-                        } else {
-                            newTemplated = undefined;
-                        }
-                        curSheet.row(r).cell(c).value(newTemplated);
-                        curSheet.row(r).cell(c)._styleId = tpltStyleId;
-
-                    }
-                    defRcnt++;
-                }
-
-            }
-            for (let i = 0; i < totalObjCnt; i++) {
-                for (let attr in data[name][i]) {
-                    if (data[name][i].hasOwnProperty(attr)) {
-                        if (i === 0) {
-                            wb.find(`%${name}.${attr}%`, data[name][i][attr]);
-                        } else {
-                            wb.find(`%${name}.${attr}.${i}%`, data[name][i][attr]);
-                        }
-                    }
-                }
-            }
-            wb.find(`%${name}:begin%`, '');
-        }
-    }
-    return wb.outputAsync('buffer')
-        .then(function (data) {
-            fs.writeFileSync('./out.xlsx', data);
-        });
+xlsxBuildByTemplate('test.xlsx', testData).then(function (data) {
+    fs.writeFileSync('./out.xlsx', data);
 });
 
 //let tpltFile = fs.readFileSync('./test.xlsx');
