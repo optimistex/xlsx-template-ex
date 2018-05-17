9a1d4202129cc34e0a5a568912cfad818d5a146a
optimistex <optimistex@gmail.com>
2018-05-17 22:45:36 +0300

    fix the test data

--- example.js
+++ example.js
@@ -57,7 +57,6 @@ const testData = {
     steps: [
         {
             stepText: 'Текст шага инспекции 1',
-            // fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.800744, 44.726850',
             media: [
                 {fileName: __dirname + '/152.jpg', created: new Date(), gpsPos: '48.2222222, 44.1111111'},
                 {fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.2222222, 44.222222222'},
@@ -69,8 +68,6 @@ const testData = {
         },
         {
             stepText: 'Текст шага инспекции 2',
-            // fileName: __dirname + '/152.jpg', created: new Date(), gpsPos: '48.800744, 44.726850',
-
             media: [
                 {fileName: __dirname + '/152.jpg', created: new Date(), gpsPos: '48.2222222, 44.1111111'},
                 {fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.2222222, 44.222222222'},
@@ -82,11 +79,9 @@ const testData = {
         },
         {
             stepText: 'Текст шага инспекции 3',
-            // fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.800744, 44.726850',
             media: [
                 {fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.33333, 44.111111'},
                 {fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.333333333, 44.22222222'},
-                // {fileName: __dirname + '/alex.jpg', created: new Date(), gpsPos: '48.800744, 44.726850'},
             ]
         },
     ]
